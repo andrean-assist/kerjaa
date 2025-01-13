96c1078f2ff02cac58e5fa75f86c74aa423447f5
Andrean Ramadhan <andrean10>
2025-01-13 17:15:46 +0700

    update: switch API endpoint to production URL in constants_connect.dart

--- lib/utils/constants_connect.dart
+++ lib/utils/constants_connect.dart
@@ -1,9 +1,9 @@
 abstract class ConstantsConnect {
   // static const endPointBaseUrl = 'http://192.168.1.163:3610/api/';
-  static const endPointBaseUrl =
-      'https://4a69-182-253-249-74.ngrok-free.app/api/';
+  // static const endPointBaseUrl =
+  //     'https://4a69-182-253-249-74.ngrok-free.app/api/';
 
-  // static const endPointBaseUrl = 'https://api-dev-hadir.assist.id/api/';
+  static const endPointBaseUrl = 'https://api-dev-hadir.assist.id/api/';
   static const endPointBaseUrlUpload = 'https://api-new-image.assist.id/';
   static const endPointBaseUrlImage =
       'https://dev-upload-file-medicaboo.s3.ap-southeast-1.amazonaws.com/';
