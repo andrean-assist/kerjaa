c320fff7015d4eee28552ba78b6c454e9aaa2b72
andrean-assist <andrean.ramadhan@assist.id>
2025-01-23 10:09:02 +0700

    update: modify Podfile.lock to upgrade Firebase dependencies, remove image_cropper, and adjust AppDelegate for Swift 5.3

--- lib/utils/constants_connect.dart
+++ lib/utils/constants_connect.dart
@@ -2,12 +2,12 @@ abstract class ConstantsConnect {
   // static const endPointBaseUrl = 'http://192.168.1.163:3610/api/';
   // static const endPointBaseUrl =
   //     'https://c333-178-128-85-216.ngrok-free.app/api/';
-
   static const endPointBaseUrl = 'https://api-dev-hadir.assist.id/api/';
+
   static const endPointBaseUrlUpload = 'https://api-new-image.assist.id/';
   static const endPointBaseUrlImage =
       'https://dev-upload-file-medicaboo.s3.ap-southeast-1.amazonaws.com/';
 
   // BUCKET
-  static const bucketUploadHospital = 'hospitalImageTest'; 
+  static const bucketUploadHospital = 'hospitalImageTest';
 }
